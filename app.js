1e0382fa431018ee040e897cc1bd5b5d5755f950
Mohammad Nizam Uddin <sha0143@hotmail.com>
2020-03-26 17:58:07 +0100

    access control change

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
    if (req.method === "OPTIONS") {
       res.header(
          "Access-Control-Allow-Methods",
-         "PUT, POST, PATCH, DELETE, GET"
+         "PUT, POST, PATCH, DELETE, GET,OPTIONS"
       );
       return res.status(200).json({});
    }
