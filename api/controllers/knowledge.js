bac1f650de84a5c02c0ebb813cb4428a45c07a5d
Mohammad Nizam Uddin <sha0143@hotmail.com>
2020-04-19 01:10:15 +0200

    knowledge controller update
    create knowledge

--- api/controllers/knowledge.js
+++ api/controllers/knowledge.js
@@ -1,37 +1,54 @@
 const mongoose = require('mongoose');
 
 const Knowledge = require('../models/knowledge');
+const User = require('../models/user');
 
 exports.knowledge_create = (req, res, next) => {
-   const knowledge = new Knowledge({
-      _id: new mongoose.Types.ObjectId(),
-      title: req.body.title,
-      description: req.body.description,
-      type: req.body.type,
-      affiliation: req.body.affiliation,
-      status: req.body.status,
-      lookingFor: req.body.lookingFor,
-      members: req.body.members,
-      knowledgeFile: req.body.knowledgeFile,
-      createdBy: req.body.createdBy,
-      comments: [],
-      likes: []
-   });
-   knowledge
-      .save()
-      .then(result => {
-         console.log(result);
-         res.status(201).json({
-            message: "Knowledge created",
-            createdKnowledge: {
-               title: result.title,
-               _id: result._id,
-               request: {
-                  type: "GET",
-                  url: process.env.BaseApiURL + "/knowledge/" + result._id
-               }
-            }
-         });
+   // first find user Id
+   User.findOne({ email: req.body.createdBy })
+      .select(
+         "_id"
+      )
+      .exec()
+      .then(user => {
+         if (user) {
+            const knowledge = new Knowledge({
+               _id: new mongoose.Types.ObjectId(),
+               title: req.body.title,
+               description: req.body.description,
+               type: req.body.type,
+               affiliation: req.body.affiliation,
+               status: req.body.status,
+               lookingFor: req.body.lookingFor,
+               members: req.body.members,
+               knowledgeFile: req.body.knowledgeFile,
+               createdBy: user._id,
+               comments: [],
+               likes: []
+            });
+            knowledge
+               .save()
+               .then(result => {
+                  console.log(result);
+                  res.status(201).json({
+                     message: "Knowledge created",
+                     createdKnowledge: {
+                        title: result.title,
+                        _id: result._id,
+                        request: {
+                           type: "GET",
+                           url: process.env.BaseApiURL + "/knowledge/" + result._id
+                        }
+                     }
+                  });
+               })
+               .catch(err => {
+                  console.log(err);
+                  res.status(500).json({ error: err });
+               });
+         } else {
+            res.status(500).json({ error: 'User not exist.' });
+         }
       })
       .catch(err => {
          console.log(err);
